60814ac6435635bfb41a95ba3ba3e843fdf17071
João Gabriel Curtinaz Maron <megajonh40@gmail.com>
2026-03-10 14:14:03 +0000

    exercicio troco

---
+++ primeiraAula/exercicio.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int valor;
+    scanf("%d", &valor);
+
+    int cem, cinq, dez, cinc, um;
+    cem = valor/100;
+    valor = valor%100;
+    cinq = valor/50;
+    valor = valor%50;
+    dez = valor/10;
+    valor = valor%10;
+    cinc = valor/5;
+    valor = valor%5;
+    um = valor/1;
+
+    printf("O valor de troco será: %d notas de 100 %d notas de 50 %d notas de 10 %d notas de 5 %d notas de 1", cem, cinq, dez, cinc, um);
+    
+}
---
+++ primeiraAula/teste.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{   int a = 13;
+    printf("Hello World\n");
+    printf("O valor de a é %d\n", a);
+    int b;
+    scanf("%d", &b);
+    printf("O valor de b é %d\n", b);
+}
